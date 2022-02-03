c5f3c64cb492e6bc5616630fbdf074cba591f29d
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-03 13:57:39 -0800

    Logistic files

---
+++ README.md
@@ -0,0 +1,1 @@
+Effect size calculations for microbiome data
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+__version__ == "0.0.1"
+
+setup(
+    name="evident",
+    author="Gibraan Rahman",
+    author_email="grahman@eng.ucsd.edu",
+    version=__version__,
+    license="BSD-3-Clause",
+    packages=find_packages()
+)
