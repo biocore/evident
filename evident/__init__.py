5b6f348ae1e5b2a69d37bf4deb78d513b0d33818
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-23 14:10:55 -0800

    post 0.0.1

--- evident/__init__.py
+++ evident/__init__.py
@@ -1,6 +1,6 @@
 from .diversity_handler import AlphaDiversityHandler, BetaDiversityHandler
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2dev"
 
 __all__ = ["AlphaDiversityHandler", "BetaDiversityHandler"]
