6dedb14ba9335d07fa0235aefc7f07a09c96dabc
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-25 16:24:09 -0800

    add pairwise, tests, & docstring

--- tests/test_exploration.py
+++ tests/test_exploration.py
@@ -15,12 +15,48 @@ def test_effect_size_by_cat():
         adh, columns=["perianal_disease", "sex", "classification",
                       "cd_behavior"]
     )
-    exp_index = ["perianal_disease", "sex", "classification",
-                 "cd_behavior"]
-    assert (df.index == exp_index).all()
+    exp_index = {"perianal_disease", "sex", "classification",
+                 "cd_behavior"}
+    assert set(df.index) == exp_index
 
     exp_cols = ["metric", "value"]
     assert (df.columns == exp_cols).all()
 
-    exp_metrics = ["cohens_d", "cohens_d", "cohens_d", "cohens_f"]
-    assert (df["metric"] == exp_metrics).all()
+    assert df.loc["perianal_disease"]["metric"] == "cohens_d"
+    assert df.loc["sex"]["metric"] == "cohens_d"
+    assert df.loc["classification"]["metric"] == "cohens_d"
+    assert df.loc["cd_behavior"]["metric"] == "cohens_f"
+
+
+def test_pairwise_effect_size_by_cat():
+    fname = os.path.join(os.path.dirname(__file__), "data/metadata.tsv")
+    df = pd.read_table(fname, sep="\t", index_col=0)
+    adh = AlphaDiversityHandler(df["faith_pd"], df)
+
+    df = expl.pairwise_effect_size_by_category(
+        adh, columns=["perianal_disease", "sex", "classification",
+                      "cd_behavior"]
+    )
+
+    exp_cols = ["column", "group_1", "group_2", "cohens_d"]
+    assert (df.columns == exp_cols).all()
+
+    all_grp_counts = df["column"].value_counts()
+    assert all_grp_counts.loc["classification"] == 1
+    assert all_grp_counts.loc["perianal_disease"] == 1
+    assert all_grp_counts.loc["sex"] == 1
+
+    cd_behavior_df = df.query("column == 'cd_behavior'")
+    assert cd_behavior_df.shape == (3, 4)
+
+    cd_behavior_melt_df = cd_behavior_df.melt(
+        value_vars=["group_1", "group_2"]
+    )
+    grp_counts = cd_behavior_melt_df["value"].value_counts()
+    grps = [
+        "Non-stricturing, non-penetrating (B1)",
+        "Stricturing (B2)",
+        "Penetrating (B3)"
+    ]
+    for grp in grps:
+        assert grp_counts.loc[grp] == 2
