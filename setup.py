3dacdf34456a29c2eeb0626b8418df1719d134f6
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-26 16:04:47 -0800

    update setup

--- setup.py
+++ setup.py
@@ -12,21 +12,40 @@ with open("evident/__init__.py", "rb") as f:
 
 q2_cmds = ["q2-evident=evident.q2.plugin_setup:plugin"]
 
+classes = """
+    Development Status :: 3 - Alpha
+    License :: OSI Approved :: BSD License
+    Topic :: Software Development :: Libraries
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Bio-Informatics
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Operating System :: Unix
+    Operating System :: POSIX
+    Operating System :: MacOS :: MacOS X
+"""
+classifiers = [s.strip() for s in classes.split("\n") if s]
+
+description = "Effect size calculations for microbiome diversity data."
+
 setup(
     name="evident",
     author="Gibraan Rahman",
     author_email="grahman@eng.ucsd.edu",
+    description=description,
+    url="https://github.com/gibsramen/evident",
     version=version,
     license="BSD-3-Clause",
     packages=find_packages(),
     install_requires=[
+        "numpy",
         "pandas",
         "statsmodels",
-        "numpy",
         "scikit-bio",
         "matplotlib",
         "seaborn"
     ],
+    classifiers=classifiers,
     extras_require={"dev": ["pytest", "pytest-cov", "flake8"]},
     entry_points={"qiime2.plugins": q2_cmds},
     package_data={"evident": ["citations.bib"]}
