172a1541a4cf56ab44fbb235bd712dd342e181a6
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-28 11:13:41 -0800

    add download links to q2 visualizations

--- evident/q2/_visualizers.py
+++ evident/q2/_visualizers.py
@@ -7,6 +7,7 @@ import pandas as pd
 from evident.plotting import plot_power_curve as ppc
 
 PATH_LOC = os.path.dirname(__file__)
+CURVE_CSS = os.path.join(PATH_LOC, "curve.css")
 TBL_CSS = os.path.join(PATH_LOC, "dataframe.css")
 
 
@@ -16,12 +17,31 @@ def plot_power_curve(
     target_power: float = 0.8,
     style: str = "alpha"
 ) -> None:
-    ppc(power_analysis_results, target_power, style, markers=True)
-    plt.savefig(os.path.join(output_dir, "power_curve.svg"))
     index_fp = os.path.join(output_dir, "index.html")
+    copy(CURVE_CSS, output_dir)
+    ppc(power_analysis_results, target_power, style, markers=True)
+
+    fig_loc = os.path.join(output_dir, "power_curve.svg")
+    fig_loc2 = os.path.join(output_dir, "power_curve.pdf")
+    plt.savefig(fig_loc)
+    plt.savefig(fig_loc2)
+
+    results_loc = os.path.join(output_dir, "results.tsv")
+    power_analysis_results.to_csv(results_loc, sep="\t", index=False)
+
     with open(index_fp, "w") as f:
         f.write("<html><body>\n")
-        f.write("<img src='power_curve.svg' alt='Power curve'>")
+        f.write("<font face='Arial'>\n")
+        f.write(
+            "<div style='text-align: center;'>\n"
+            "<a href='power_curve.pdf' target='_blank'"
+            "rel='noopener noreferrer'>"
+            "Download plot as PDF</a><br>\n"
+            "<a href='results.tsv'>Download results as TSV</a><br>\n"
+        )
+        f.write("<img src='power_curve.svg' alt='Power curve'>\n")
+        f.write("</div>\n")
+        f.write("</font>")
 
 
 def visualize_results(
