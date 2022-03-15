b264ec2d7480178a558104a468deaf0431184fc7
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-03-15 10:06:46 -0500

    Add docstring to interactive function

--- evident/interactive.py
+++ evident/interactive.py
@@ -8,18 +8,39 @@ from evident.diversity_handler import (_BaseDiversityHandler,
                                        BetaDiversityHandler)
 
 
-def create_bokeh_app(diversity_handler: _BaseDiversityHandler,
-                     output: os.PathLike, max_levels_per_category: int = 5,
-                     exist_ok: bool = False) -> None:
+def create_bokeh_app(
+    diversity_handler: _BaseDiversityHandler,
+    output: os.PathLike,
+    max_levels_per_category: int = 5,
+    exist_ok: bool = False
+) -> None:
+    """Creates interactive power analysis using bokeh.
+
+    :param diversity_handler: Handler with diversity data
+    :type diversity_handler: evident.diversity_handler._BaseDiversityHandler
+
+    :param output: Location to create bokeh app
+    :type output: os.PathLike
+
+    :param max_levels_per_category: Max number of levels in a category to
+        keep. Any categorical columns that have more than this number of
+        unique levels will not be saved, defaults to 5.
+    :type max_levels_per_category: int
+
+    :param exist_ok: Whether to allow intermediate directories to be created
+        and existing data to be overwritten, defaults to False
+    :type exist_ok: bool
+    """
     curr_path = os.path.dirname(__file__)
     support_files = os.path.join(curr_path, "support_files")
 
+    # Copy support files (bokeh template + script) and data directory
     shutil.copytree(support_files, output, dirs_exist_ok=exist_ok)
     data_dir = os.path.join(output, "data")
     os.makedirs(data_dir, exist_ok=exist_ok)
 
     md = diversity_handler.metadata
-    # Get all categorical columns
+    # Get all valid categorical columns and save metadata to data dir
     valid_cols = [
         x for x in md.columns
         if md[x].dtype == np.dtype("object")
