444dc39c7c948951384f7d54e5b40d3b2132c480
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-04-25 15:58:50 -0700

    update to 0.3.0

--- evident/__init__.py
+++ evident/__init__.py
@@ -1,6 +1,6 @@
 from .diversity_handler import AlphaDiversityHandler, BetaDiversityHandler
 
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["AlphaDiversityHandler", "BetaDiversityHandler"]
