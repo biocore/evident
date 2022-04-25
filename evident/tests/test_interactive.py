d350ead8c03233b4d4d27647b4448bdf85d55185
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-04-25 12:51:27 -0700

    Update test_interactive to not check for warnings

--- evident/tests/test_interactive.py
+++ evident/tests/test_interactive.py
@@ -10,14 +10,10 @@ from evident.interactive import create_bokeh_app
 
 @pytest.mark.parametrize("mock", ["alpha_mock", "beta_mock"])
 def test_interactive(mock, request, tmpdir):
-    dh = request.getfixturevalue(mock)
-    na_vals = ["missing: not provided", "not applicable"]
-    dh.metadata = dh.metadata.replace({x: np.nan for x in na_vals})
-    dh.metadata = dh.metadata.dropna(axis=1, how="all")
-    dh.metadata = dh.metadata.dropna(axis=0, how="any")
-
     outdir = f"{tmpdir}/{mock}"
+
     with pytest.warns(UserWarning) as warn_info:
+        dh = request.getfixturevalue(mock)
         create_bokeh_app(dh, f"{outdir}")
 
     exp_msg_1 = (
@@ -25,7 +21,7 @@ def test_interactive(mock, request, tmpdir):
         "one level or too many. Use the max_levels_per_category "
         "argument to modify this threshold."
     )
-    assert warn_info[0].message.args[0] == exp_msg_1
+    assert exp_msg_1 in warn_info[0].message.args[0]
 
     files = glob.glob(f"{outdir}/**/*", recursive=True)
     files = set([os.path.relpath(x, start=outdir) for x in files])
@@ -55,47 +51,3 @@ def test_interactive(mock, request, tmpdir):
         "classification"
     ]
     assert (exp_cols == md.columns).all()
-
-
-@pytest.mark.parametrize("mock", ["alpha_mock", "beta_mock"])
-def test_interactive_warnings(mock, request, tmpdir):
-    dh = request.getfixturevalue(mock)
-    na_vals = ["missing: not provided", "not applicable"]
-    dh.metadata = dh.metadata.replace({x: np.nan for x in na_vals})
-    dh.metadata = dh.metadata.dropna(axis=1, how="all")
-    dh.metadata = dh.metadata.dropna(axis=0, how="any")
-
-    col_1 = ["A", "A", "A", "C", "C"] + ["B"] * (dh.metadata.shape[0] - 5)
-    dh.metadata["col_1"] = col_1
-
-    outdir = f"{tmpdir}/{mock}"
-    with pytest.warns(UserWarning) as warn_info:
-        create_bokeh_app(dh, f"{outdir}")
-
-    exp_msg_1 = (
-        "Some categories have been dropped because they had either only "
-        "one level or too many. Use the max_levels_per_category "
-        "argument to modify this threshold."
-    )
-    exp_msg_2 = (
-        "Some categorical levels have been dropped because they "
-        "did not have enough samples. Use the min_count_per_level "
-        "argument to modify this threshold."
-    )
-    assert warn_info[0].message.args[0] == exp_msg_1
-    assert warn_info[1].message.args[0] == exp_msg_2
-
-    md = pd.read_table(os.path.join(outdir, "data/metadata.tsv"), sep="\t",
-                       index_col=0)
-    exp_cols = [
-        "cd_behavior",
-        "cd_location",
-        "cd_resection",
-        "ibd_subtype",
-        "perianal_disease",
-        "sex",
-        "classification",
-        "col_1"
-    ]
-    assert (exp_cols == md.columns).all()
-    assert set(md["col_1"].dropna().unique()) == {"A", "B"}
